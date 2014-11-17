31234132d44576449fc380f81f234496974e13e8
devintjones <devin.thatcher.jones@gmail.com>
2014-11-17 18:21:32 -0500

    whopsie daisy!

--- pricing.R
+++ pricing.R
@@ -22,8 +22,35 @@ overPrices <- data.frame(price=c(20:60),prof=prof)
 plot(overPrices)
 
 
-library(RCurl)
+quotes <- c("GOOG","AAPL")
 
+selectedTags <- c("Name","Symbol","Ask","Bid")
 
+getQuotes <- function(quotes,selectedTags){
+  require(RCurl)
+  
+  props <- read.delim("~/r_workspace/props.txt",stringsAsFactors=FALSE)
+  
+  selectedTags <- data.frame(name=selectedTags)
+  tags <- merge(selectedTags,props,by="name")
+  
+  url <- paste0("http://download.finance.yahoo.com/d/quotes.csv?s="
+                ,paste(quotes,collapse=",")
+                ,"&f="
+                ,paste(tags$tag,collapse="")
+                ,"&e=.csv")
+  
+  response        <- getURL(url)
+  #clean data
+  responseCln     <- gsub("\\\\|\"|\n","",response)
+  rows            <- data.frame(strsplit(responseCln,"\r")[[1]],stringsAsFactors=FALSE)
+  rowList         <- sapply(rows[,1],function(x) strsplit(x,","))
+  data            <- data.frame(do.call("rbind",rowList),stringsAsFactors=FALSE)
+  names(data)     <- tags$name
+  row.names(data) <- NULL
+  data$Ask        <- as.numeric(data$Ask)
+  data$Bid        <- as.numeric(data$Bid)
+  return(data)
+}
 
-
+data <- getQuotes(quotes,selectedTags)
